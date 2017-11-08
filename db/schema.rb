f855cd43eac0404b768e65fc4d75516c38fa1d6e
gon.fervic <gon.fervic@gmail.com>
2017-11-08 01:22:23 +0000

    adding content to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031114834) do
+ActiveRecord::Schema.define(version: 20171108011328) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string   "name", null: false
-    t.integer  "priority", default: 0
+    t.string   "name"
+    t.integer  "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "content"
   end
 
 end
